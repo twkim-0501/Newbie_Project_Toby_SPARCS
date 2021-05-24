094010ae88a3c425223f241a64ac15df8d5a4f8b
Toby <rlaxodntttt@kaist.ac.kr>
2021-05-25 00:25:37 +0900

    Practice simple JSX(style)

--- start-react-app/src/App.js
+++ start-react-app/src/App.js
@@ -1,6 +1,16 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+//import logo from './logo.svg';
 import './App.css';
-
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        hi there
+      </div>
+    );
+  }
+}
+/*
 function App() {
   return (
     <div className="App">
@@ -20,6 +30,6 @@ function App() {
       </header>
     </div>
   );
-}
+}*/
 
 export default App;
