f6e313b1162b5e7f1b00bc908994a9a69a39107b
Toby <rlaxodntttt@kaist.ac.kr>
2021-05-25 22:24:27 +0900

    Understanding state(by Counter.js)

--- start-react-app/src/App.js
+++ start-react-app/src/App.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
-import MyName from './MyName';
+import Counter from './Counter';
 import './App.css';
 class App extends Component {
   render() {
     return (
-      <MyName name="김태우"/>
+      <Counter/>
     );
   }
 }
