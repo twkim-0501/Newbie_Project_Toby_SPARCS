1b1bf9021b0d147db58b991a8e6d85a5fd9ee20a
Toby <rlaxodntttt@kaist.ac.kr>
2021-05-26 02:20:21 +0900

    Making phone-book_2(practice managing Array)

--- start-react-app/src/App.js
+++ start-react-app/src/App.js
@@ -1,16 +1,37 @@
 import React, {Component} from 'react';
 import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList';
 
 class App extends Component {
+  id = 2
+  state = {
+    information: [
+      {
+        id: 0,
+        name: '김 태 우',
+        phone: '010-4742-2687'
+      },
+      {
+        id: 1,
+        name: '김 태 양',
+        phone: '010-7613-2687'
+      }
+    ]
+  }
   handleCreate = (data) => {
-    console.log(data);
+    const {information} = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data})
+    })
   }
   render() {
+    const {information} = this.state;
     return (
       <div>
         <PhoneForm 
           onCreate={this.handleCreate}
         />
+        <PhoneInfoList data={this.state.information}/>
       </div>
     );
   }
